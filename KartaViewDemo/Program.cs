720d91cec20115f5142e85511460db19d1ca57dc
Pierce Andjelkovic <pierceandjelkovic@gmail.com>
2023-12-31 16:02:39 +1100

    Bring back demo code

--- KartaViewDemo/Program.cs
+++ KartaViewDemo/Program.cs
@@ -1,29 +1,32 @@
-﻿namespace KartaView
+﻿using KartaViewSharp.V2;
+using KartaViewSharp.V2.Request;
+
+namespace KartaView
 {
 	internal static class Program
 	{
 		static async Task Main()
 		{
-			//const int sequenceId = 7068281;
+			const int sequenceId = 7068281;
 
-			// Initialize the API client
-			//var apiClient = new Client();
+			//Initialize the API client
+			var apiClient = new Client();
 
-			//// Make the API request
-			//var result = await apiClient.RetrievePhotos(new PhotoQueryFilters
-			//{
-			//	SequenceId = sequenceId
-			//});
+			// Make the API request
+			var result = await apiClient.RetrievePhotos(new PhotoQueryFilters
+			{
+				SequenceId = sequenceId
+			});
 
-			//// Process the result
-			//Console.WriteLine(result.Result.Data.FirstOrDefault()?.Name);
+			// Process the result
+			Console.WriteLine(result.Result.Data.FirstOrDefault()?.Name);
 
-			//var result2 = await apiClient.RetrieveSequences(new SequenceQueryFilters()
-			//{
-			//	Id = [sequenceId]
-			//});
+			var result2 = await apiClient.RetrieveSequences(new SequenceQueryFilters()
+			{
+				Id = [sequenceId]
+			});
 
-			//Console.WriteLine(result2.Result.Data.FirstOrDefault()?.Address);
+			Console.WriteLine(result2.Result.Data.FirstOrDefault()?.Address);
 
 			var client = new KartaViewSharp.V1.Client();
 
